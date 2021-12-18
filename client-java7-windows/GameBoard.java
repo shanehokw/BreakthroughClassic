65617a4bd3ebae7b860770ea6e553a2347794670
jerome200020 <64579876+jerome200020@users.noreply.github.com>
2021-12-18 16:19:08 +0800

    Update GameBoard.java

--- client-java7-windows/GameBoard.java
+++ client-java7-windows/GameBoard.java
@@ -241,42 +241,26 @@ public class GameBoard extends JFrame implements ActionListener{
 	}
 	
 	public void setColor(int row, int col) {
-		if ((row == 3 || row == 4) && (col == 0 || col == 7)) {
+		if ((row == 0 || row == 2 || row == 4 || row == 6) && (col == 0 || col == 2 || col == 4 || col == 6)) {
 			buttons[row][col].setBackground(Color.BLACK);
-			//fixes for mac compatibility
 			buttons[row][col].setOpaque(true);
 			buttons[row][col].setBorderPainted(false);
-		}
-		else if ((row == 2 || row == 5) && (col == 1 || col == 6)) {
+		}//set black blocks for all even numbered rows
+		else if ((row == 1 || row == 3 || row == 5 || row == 7) && (col == 1 || col == 3 || col == 5 || col == 7)) {
 			buttons[row][col].setBackground(Color.BLACK);
-			//fixes for mac compatibility
 			buttons[row][col].setOpaque(true);
 			buttons[row][col].setBorderPainted(false);
-		}
-		else if ((row == 1 || row == 6) && (col == 2 || col == 5)) {
-			buttons[row][col].setBackground(Color.BLACK);
-			//fixes for mac compatibility
-			buttons[row][col].setOpaque(true);
-			buttons[row][col].setBorderPainted(false);
-		}
-		else if ((col == 3 || col == 4) && (row == 0 || row == 7)) {
-			buttons[row][col].setBackground(Color.BLACK);
-			//fixes for mac compatibility
-			buttons[row][col].setOpaque(true);
-			buttons[row][col].setBorderPainted(false);
-		}
-		else if ((row == 4 || row == 5) && (col == 1 || col == 2 || col == 3 || col == 4 || col == 5 || col == 6) || (row == 3 && (col == 3 || col == 4)) || (row == 6 && (col == 3 || col == 4))) {
+		}//set black blocks for all odd numbered rows
+		else if ((row == 0 || row == 2 || row == 4 || row == 6) && (col == 1 || col == 3 || col == 5 || col == 7)) {
 			buttons[row][col].setBackground(Color.WHITE);
-			//fixes for mac compatibility
 			buttons[row][col].setOpaque(true);
 			buttons[row][col].setBorderPainted(false);
-		}
-		else if ((row == 1 && (col == 3 || col == 4)) || (row == 2 && (col == 2 || col == 3 || col == 4 || col == 5)) || (row == 3 && (col == 1 || col == 2 || col == 5 || col == 6))) {
-			buttons[row][col].setBackground(Color.RED);
-			//fixes for mac compatibility
+		}//set white blocks for all even numbered rows
+		else if ((row == 1 || row == 3 || row == 5 || row == 7) && (col == 0 || col == 2 || col == 4 || col == 6)) {
+			buttons[row][col].setBackground(Color.WHITE);
 			buttons[row][col].setOpaque(true);
 			buttons[row][col].setBorderPainted(false);
-		}
+		}//set white blocks for all odd numbered rows
 	}
 	
 	public void actionPerformed(ActionEvent ae) {
